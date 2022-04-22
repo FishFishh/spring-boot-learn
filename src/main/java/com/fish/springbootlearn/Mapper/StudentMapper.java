03c1551f9c7d548bde512779de24f710c0babd1b
fish214057 <2140579318@qq.com>
2022-04-22 21:33:06 +0800

    wuhu

--- src/main/java/com/fish/springbootlearn/Mapper/StudentMapper.java
+++ src/main/java/com/fish/springbootlearn/Mapper/StudentMapper.java
@@ -1,10 +1,12 @@
 package com.fish.springbootlearn.Mapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 /**
  * Student Mapper类
  */
 @Mapper
-@
+@Repository
 public interface StudentMapper {
 
 }
